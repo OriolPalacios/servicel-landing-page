45f1b671709ce9b862f644cc72388fdfb289c2ea
Oriol Palacios <orioloriolpalacios@outlook.com>
2024-11-14 09:41:32 -0500

    corerspondign files added

---
+++ assets/js/anime-test.js
@@ -0,0 +1,18 @@
+var tl = anime.timeline({});
+
+tl
+    .add({
+        targets: ['.fil1', '.fil2', '.fil3', '.fil4'],
+        strokeDashoffset: [anime.setDashoffset, 0],
+        easing: 'easeInOutSine',
+        duration: 1500,
+        delay: function (el, i) { return i * (400) },
+    })
+    .add({
+        targets: ['.fil1', '.fil2', '.fil3', '.fil4'],
+        fill: "#dbcf21",
+        duration: 500,
+        delay: anime.stagger(200),
+        easing: 'easeInOutSine',
+    })
+
